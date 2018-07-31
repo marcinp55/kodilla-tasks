931d9a65d7d63abf142a57a78c286e6a19e98bd2
“marcinp55 <marcinp55@gmail.com>
2018-07-31 18:35:50 +0200

    JAR and PostgreSQL fixed heroku 2

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -9,13 +9,13 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 //public class TasksApplication extends SpringBootServletInitializer
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
-	}
+	//@Override
+	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	//	return application.sources(TasksApplication.class);
+	//}
 }
